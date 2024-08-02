c65bff64556c9c4edf852131cdcc5deeb8baddeb
Ray Mascarena <164954127+RayMascarena@users.noreply.github.com>
2024-08-02 16:13:22 -0700

    Update checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart, updateQuantity, calculateCartQuantity} from './cart.js';
+import {cart, removeFromCart, calculateCartQuantity} from './cart.js';
 import {products} from './products.js';
 
 let cartSummaryHTML = '';
@@ -27,11 +27,6 @@ cart.forEach((cartItem) => {
             <div></div>
             <span class="delete-quantity-link link-primary js-delete-link" 
                 data-product-id="${matchingProduct.id}">Delete</span>
-            <span class="update-quantity-link link-primary js-update-link"
-               data-product-id="${matchingProduct.id}">Update</span>
-            <input class="quantity-input js-quantity-input-${matchingProduct.id}">
-            <span class="save-quantity-link link-primary js-save-link"
-               data-product-id="${matchingProduct.id}">Save</span>
             </div>
         </div>
     `;
@@ -58,46 +53,3 @@ function updateCartQuantity() {
 }
 
 updateCartQuantity();
-
-
-document.querySelectorAll('.js-update-link')
-    .forEach((link) => {
-        link.addEventListener('click', () => {
-            const productId = link.dataset.productId;
-        
-            const container = document.querySelector(
-            `.js-cart-item-container-${productId}`);
-
-            container.classList.add('is-editing-quantity');
-        });
-    });
-
-document.querySelectorAll('.js-save-link')
-    .forEach((link) => {
-        link.addEventListener('click', () => {
-            const productId = link.dataset.productId;
-
-            const quantityInput = document.querySelector(
-            `.js-quantity-input-${productId}`);
-
-            const newQuantity = Number(quantityInput.value);
-
-            if (newQuantity < 0 || newQuantity >= 1000) {
-                alert('Quantity must be at least 0 and less than 1000');
-                return;
-            }
-
-            updateQuantity(productId, newQuantity);
-
-            const container = document.querySelector(
-            `.js-cart-item-container-${productId}`);
-
-            container.classList.remove('is-editing-quantity');
-
-            const quantityLabel = document.querySelector(
-                `.js-quantity-label-${productId}`);
-                
-            quantityLabel.innerHTML = newQuantity;
-            updateCartQuantity();
-        });
-    });
