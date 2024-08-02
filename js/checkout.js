b8202181da7467b689f34e0774f4309d3a0a2c52
Ray Mascarena <164954127+RayMascarena@users.noreply.github.com>
2024-08-02 14:45:06 -0700

    Update checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -43,15 +43,19 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
 
         const container = document.querySelector(`.js-cart-item-container-${productId}`);
         container.remove();
+        updateCartQuantity();
     });
 });
 
+function updateCartQuantity() {
+    let cartQuantity = 0;
+    
+    cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+    });
+    
+    document.querySelector('.js-quantity-display')
+        .innerHTML = `(${cartQuantity} items)`;
+}
 
-let cartQuantity = 0;
-
-cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-});
-
-document.querySelector('.js-quantity-display')
-    .innerHTML = `(${cartQuantity} items)`;
+updateCartQuantity();
