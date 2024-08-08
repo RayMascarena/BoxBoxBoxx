f6b09ec9683ffaabfda55ab1f29106a407ec4d48
Ray Mascarena <164954127+RayMascarena@users.noreply.github.com>
2024-08-08 14:45:52 -0700

    Update homeproducts.js

--- js/homeproducts.js
+++ js/homeproducts.js
@@ -6,7 +6,9 @@ let productsHTML = '';
 products.slice(0, 4).forEach((product) => {
     productsHTML += `
         <div class="product-box">
-            <img class="product-img" src="${product.image}">
+            <a href="https://raymascarena.github.io/Products/${product.image}" target="_blank">
+                <img class="product-img" src="${product.image}">
+            </a>
             <h2 class="product-title">${product.name}</h2>
             <div class="space-between">
                 <span class="price">$${(Math.round(product.priceCents) / 100).toFixed(2)}</span>
